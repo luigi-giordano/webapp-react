7a7ea8a1b0f5cc376a26c7b08f083de2178d3135
luigi-giordano <luigi.giordano1992@gmail.com>
2025-02-07 16:40:47 +0100

    import API_URL

--- context/GlobalContext.jsx
+++ context/GlobalContext.jsx
@@ -1,11 +1,20 @@
-import { Children, createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import axios from "axios";
 
 const GlobalContext = createContext()
 
 const GlobalProvider = ({ children }) => {
 
-  const value = {}
+  const api_url = import.meta.env.VITE_API_URL
+
+  const fetchMovies = () => {
+    console.log(api_url);
+
+  }
+
+  const value = {
+    fetchMovies
+  }
 
   return (
     <GlobalContext.Provider value={value}>
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,5 +1,12 @@
+import { useGlobalContext } from "../context/GlobalContext"
+import { useEffect } from "react"
 
 function HomePage() {
+
+  const { fetchMovies } = useGlobalContext()
+
+  useEffect(fetchMovies, [])
+
   return (
     <div>
       Home
